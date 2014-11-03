59400dade9c4841a61cdde476ec3a33bd5492ab2
Patrick <Erdnuss.1988@hotmail.de>
2014-11-03 16:10:50 +0100

    Updated Product List View

---
+++ WebContent/json/nav_products_list.js
@@ -0,0 +1,52 @@
+var myArray = [
+	{
+		"pTitle" : "Product Title ABC",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title CDE",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title FGH",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title IJK",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title LMN",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title OPQ",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title RST",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title UVW",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title XYZ",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+	{
+		"pTitle" : "Product Title XYZ",
+		"pDescription" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+		"pPrice" : "$21.000"
+	},
+]
