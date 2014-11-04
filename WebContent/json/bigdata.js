dd8483f5657d0fcc7a0cc05084bb310aa4e89fa2
schindlerc <schindlerc492@googlemail.com>
2014-11-04 14:44:17 +0100

    bigdata.json

---
+++ WebContent/json/bigdata.js
@@ -0,0 +1,32 @@
+var myArray = [
+	{
+		"heading" : "A new Farmer joint Barnbay",
+		"news" : "Welcome Farmer John, he is new at Barnbay",
+		"img" : "",
+	},
+	{
+		"heading" : "New in Stock",
+		"news" : "Farmer Jack just updated his Stock and added 200 kg Potatos",
+		"img" : "",
+	},
+	{
+		"heading": "New Location",
+		"news" : "We have a new Pick-up Location at the University of Mannheim",
+		"img" : "",
+	},
+	{
+		"heading": "New Location",
+		"news" : "We have a new Pick-up Location at the SAP Headquarter (Walldorf)",
+		"img" : "",
+	},
+	{
+		"heading" : "New in Stock",
+		"news" : "Farmer Paul just updated his Stock and added 50 kg Apples",
+		"img" : "",
+	},
+	{
+		"heading" : "First Purchase",
+		"news" : "[User] trustes Barnbay and placed his first Order",
+		"img" : "",
+	},
+]
