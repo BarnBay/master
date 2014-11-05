63f72ec0cca1502d47c25cc081665e0c99d35e57
Patrick <Erdnuss.1988@hotmail.de>
2014-11-05 14:55:29 +0100

    BigData Activity

--- WebContent/json/bigdata.js
+++ WebContent/json/bigdata.js
@@ -1,32 +1,37 @@
-var myArray = [
+var myBigDataArray = [
 	{
-		"heading" : "A new Farmer joint Barnbay",
-		"news" : "Welcome Farmer John, he is new at Barnbay",
-		"img" : "",
+		heading : "A new Farmer joint Barnbay",
+		news : "Welcome Farmer John, he is new at Barnbay",
+		img : "",
 	},
 	{
-		"heading" : "New in Stock",
-		"news" : "Farmer Jack just updated his Stock and added 200 kg Potatos",
-		"img" : "",
+		heading : "New in Stock",
+		news : "Farmer Jack just updated his Stock and added 200 kg Potatos",
+		img : "",
 	},
 	{
-		"heading": "New Location",
-		"news" : "We have a new Pick-up Location at the University of Mannheim",
-		"img" : "",
+		heading: "New Location",
+		news : "We have a new Pick-up Location at the University of Mannheim",
+		img : "",
 	},
 	{
-		"heading": "New Location",
-		"news" : "We have a new Pick-up Location at the SAP Headquarter (Walldorf)",
-		"img" : "",
+		heading: "New Location",
+		news : "We have a new Pick-up Location at the SAP Headquarter (Walldorf)",
+		img : "",
 	},
 	{
-		"heading" : "New in Stock",
-		"news" : "Farmer Paul just updated his Stock and added 50 kg Apples",
-		"img" : "",
+		heading : "New in Stock",
+		news : "Farmer Paul just updated his Stock and added 50 kg Apples",
+		img : "",
 	},
 	{
-		"heading" : "First Purchase",
-		"news" : "[User] trustes Barnbay and placed his first Order",
-		"img" : "",
+		heading : "First Purchase",
+		news : "[User X] trustes Barnbay and placed his first Order",
+		img : "",
+	},
+	{
+		heading : "First Purchase",
+		news : "[User Z] trustes Barnbay and placed his first Order",
+		img : "",
 	},
 ]
