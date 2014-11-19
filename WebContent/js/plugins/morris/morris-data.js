49abd6b813b3e91866829ed99111083f6920a9f2
Marcel Reinsch <marcel.reinsch@sap.com>
2014-11-19 22:58:14 +0100

    Changed sales values (chart values)

--- WebContent/js/plugins/morris/morris-data.js
+++ WebContent/js/plugins/morris/morris-data.js
@@ -3,59 +3,59 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2012 Q3',
+            sales_product: 2666,
+            sales_product2: null,
+            sales_product3: 2647
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2012 Q4',
+            sales_product: 2778,
+            sales_product2: 2294,
+            sales_product3: 2441
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2013 Q1',
+            sales_product: 4912,
+            sales_product2: 1969,
+            sales_product3: 2501
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2013 Q2',
+            sales_product: 3767,
+            sales_product2: 3597,
+            sales_product3: 5689
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2013 Q3',
+            sales_product: 6810,
+            sales_product2: 1914,
+            sales_product3: 2293
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2013 Q4',
+            sales_product: 5670,
+            sales_product2: 4293,
+            sales_product3: 1881
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2014 Q1',
+            sales_product: 4820,
+            sales_product2: 3795,
+            sales_product3: 1588
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2014 Q2',
+            sales_product: 15073,
+            sales_product2: 5967,
+            sales_product3: 5175
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2014 Q3',
+            sales_product: 10687,
+            sales_product2: 4460,
+            sales_product3: 2028
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2014 Q4',
+            sales_product: 8432,
+            sales_product2: 5713,
+            sales_product3: 1791
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['sales_product', 'sales_product2', 'sales_product3'],
+        labels: ['Sales Apples', 'Sales Potatoes', 'Sales Carrots'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
