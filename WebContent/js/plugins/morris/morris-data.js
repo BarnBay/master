aa457974238c0b6f18577f9d9654bdc959bd7aa5
Marcel Reinsch <marcel.reinsch@sap.com>
2014-11-19 17:23:21 +0100

    Fixed bug that sales dissappear

--- WebContent/js/plugins/morris/morris-data.js
+++ WebContent/js/plugins/morris/morris-data.js
@@ -64,54 +64,17 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
-            value: 12
+            label: "University of Mannheim",
+            value: 210
         }, {
-            label: "In-Store Sales",
-            value: 30
+            label: "Mannheim Mainstation",
+            value: 122
         }, {
-            label: "Mail-Order Sales",
+            label: "SAP SE",
             value: 200
         }],
         resize: true
     });
 
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
-        hideHover: 'auto',
-        resize: true
-    });
 
 });
