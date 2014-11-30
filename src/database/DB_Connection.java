26ec5e50e9c440dc5e34852b8030294431b7504a
ada91 <codingada@gmail.com>
2014-11-30 13:20:36 +0100

    added javadoc the the DB_Connection class

--- src/database/DB_Connection.java
+++ src/database/DB_Connection.java
@@ -9,10 +9,24 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+/**
+ * 
+ * @author Anne
+ * This class manages the connection to the database (local Derby
+ *  or SAP HANA) and offers methods that are used by the services
+ */
 public class DB_Connection {
 
 	Connection con;
 
+	/** The constructor takes the initial context (the initial context 
+	 * implements the Context interface and provides the starting point 
+	 * for resolution of names) and looks up the data source,
+	 * then it gets the connection to the datasource
+	 * 
+	 * -> here is the inital touch to the database (HANA or Derby) done!
+	 * 
+	 */
 	public DB_Connection() {
 		InitialContext ctx;
 		try {
@@ -30,6 +44,11 @@ public class DB_Connection {
 
 	}
 	
+	/** Update table method which returns true or false
+	 * 
+	 * @param sql SQL-String
+	 * @return true or false indicating whether the operation succeeded
+	 */
 	public boolean executeSQLbool(String sql) {
 		int res = 0;
 		try {
@@ -47,6 +66,13 @@ public class DB_Connection {
 		}
 	}
 
+	/** This method takes an SQL-String,
+	 * executes it agains the database
+	 * and returns and SQL-ResultSet
+	 * 
+	 * @param sql SQL-String
+	 * @return ResultSet
+	 */
 	public ResultSet executeSQL(String sql) {
 		ResultSet rs = null;
 		try {
@@ -61,6 +87,9 @@ public class DB_Connection {
 		return rs;
 	}
 	
+	/**
+	 * This method closes the connection to the db
+	 */
 	public void close(){
 		try {
 			this.con.close();
@@ -70,6 +99,14 @@ public class DB_Connection {
 		}
 	}
 	
+	/**
+	 * This method is used for distinguishing
+	 * between the local test-schema and the actual
+	 * schema on the SAP HANA Cloud Platform according to the URL
+	 * 
+	 * @param url url-String
+	 * @return schema name as String
+	 */
 	public static String getSchemaName(String url){
 		String s;
 		if(url.indexOf("ondemand.com") > -1){
